4aed8818c7294d1f46c17ceacdddf4f6c3d274c9
cbui9 <documentationangularmvc@gmail.com>
2017-02-12 15:58:45 +0700

    add

---
+++ src/categories.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+    templateUrl:"src/categories.html",
+
+})
+export class Categories{
+
+}
---
+++ src/securityRoute.ts
@@ -0,0 +1,17 @@
+import {NgModule} from "@angular/core";
+import {RouterModule ,Routes} from "@angular/router";
+import {Categories} from "./categories";
+
+let routes: Routes =[
+    {path: "", redirectTo: "categories", pathMatch: "full"},
+    
+    {path: "categories", component: Categories}
+];
+
+@NgModule({
+    imports:[RouterModule.forRoot(routes)],
+    exports:[RouterModule]
+})
+export class SecurityRoute{
+
+}
