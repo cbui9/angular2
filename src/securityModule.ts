e8d4f18fba4d7e4ec9dd7272b62b090bc774fde4
cbui9 <documentationangularmvc@gmail.com>
2017-02-12 15:36:49 +0700

    define routing

--- src/securityModule.ts
+++ src/securityModule.ts
@@ -2,9 +2,12 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { DefaultPage } from "./defaultPage";
+import {SecurityRoute} from "./securityRoute";
+import {Categories} from "./categories";
+
 @NgModule({
-    imports: [BrowserModule, FormsModule],
-    declarations: [DefaultPage],
+    imports: [BrowserModule, FormsModule, SecurityRoute],
+    declarations: [DefaultPage, Categories],
     bootstrap: [DefaultPage]
 })
 export class SecurityModule { }
